5ab1514d377ead5928908cbd9b06062bd6a7fdc1
jhiven <jivenagnar11@gmail.com>
2024-05-31 19:44:05 +0700

    feat: filter every place must be greater than 20m distance

--- index.js
+++ index.js
@@ -1,7 +1,29 @@
 const axios = require("axios").default;
 const fs = require("fs").promises;
 
-const areaIdList = ["3608225175", "3608225259"];
+const areaIdList = ["3608225259", "3608225592"];
+const minDistance = 20;
+
+//
+// Warn if overriding existing method
+if (Array.prototype.equals)
+  console.warn(
+    "Overriding existing Array.prototype.equals. Possible causes: New API defines the method, there's a framework conflict or you've got double inclusions in your code.",
+  );
+Array.prototype.equals = function (array) {
+  if (!array) return false;
+  if (array === this) return true;
+  if (this.length != array.length) return false;
+  for (var i = 0, l = this.length; i < l; i++) {
+    if (this[i] instanceof Array && array[i] instanceof Array) {
+      if (!this[i].equals(array[i])) return false;
+    } else if (this[i] != array[i]) {
+      return false;
+    }
+  }
+  return true;
+};
+Object.defineProperty(Array.prototype, "equals", { enumerable: false });
 
 function getQuery(id) {
   const query = `/*
@@ -16,14 +38,13 @@ area(id:${id})->.searchArea;
   node["building"]["name"](area.searchArea);
   way["building"]["name"](area.searchArea);
   node["amenity"]["name"](area.searchArea);
-  way["amenity"]["name"](area.searchArea);
   node["shop"]["name"](area.searchArea);
-  way["shop"]["name"](area.searchArea);
   node["office"]["name"](area.searchArea);
-  way["office"]["name"](area.searchArea);
   node["place"]["name"](area.searchArea);
   way["place"]["name"](area.searchArea);
   node["public_transport"]["name"](area.searchArea);
+  node["church"]["name"](area.searchArea);
+  node["mosque"]["name"](area.searchArea);
   way["public_transport"]["name"](area.searchArea);
 );
 // print results
@@ -32,7 +53,53 @@ out center;`;
   return query;
 }
 
-async function getPlace() {
+const haversineDistance = ([lat1, lon1], [lat2, lon2]) => {
+  const RADIUS_OF_EARTH_IN_KM = 6371;
+  const toRadian = (angle) => (Math.PI / 180) * angle;
+  const distance = (a, b) => (Math.PI / 180) * (a - b);
+
+  const dLat = distance(lat2, lat1);
+  const dLon = distance(lon2, lon1);
+
+  lat1 = toRadian(lat1);
+  lat2 = toRadian(lat2);
+
+  const a =
+    Math.pow(Math.sin(dLat / 2), 2) +
+    Math.pow(Math.sin(dLon / 2), 2) * Math.cos(lat1) * Math.cos(lat2);
+  const c = 2 * Math.asin(Math.sqrt(a));
+
+  let finalDistance = RADIUS_OF_EARTH_IN_KM * c;
+
+  return finalDistance * 1000;
+};
+
+const checkDistance = ([lat1, lon1], [lat2, lon2]) => {
+  const distance = haversineDistance([lat1, lon1], [lat2, lon2]);
+
+  if (distance > minDistance) {
+    console.log(
+      "[greater] coor1: ",
+      [lat1, lon1],
+      "coor2: ",
+      [lat2, lon2],
+      "distance: ",
+      distance,
+    );
+    return true;
+  }
+  console.log(
+    "[lesser] coor1: ",
+    [lat1, lon1],
+    "coor2: ",
+    [lat2, lon2],
+    "distance: ",
+    distance,
+  );
+  return false;
+};
+
+const getPlace = async () => {
   try {
     const json = [];
     console.log("Fetching overpass api data");
@@ -54,7 +121,7 @@ async function getPlace() {
     }
 
     await fs.writeFile(
-      "result.json",
+      "result_orig.json",
       JSON.stringify(json, null, 2),
       (error) => {
         if (error) {
@@ -62,10 +129,63 @@ async function getPlace() {
         }
       },
     );
-    console.log("Data has been written to result.json");
+    return json;
   } catch (error) {
     console.error("Error fetching data from Overpass API:", error);
   }
-}
+};
 
-getPlace();
+const getLatLon = (item) => {
+  if (item.lat !== undefined && item.lon !== undefined) {
+    return [item.lat, item.lon];
+  } else if (
+    item.center !== undefined &&
+    item.center.lat !== undefined &&
+    item.center.lon !== undefined
+  ) {
+    return [item.center.lat, item.center.lon];
+  }
+  return null;
+};
+
+const main = async () => {
+  await getPlace();
+
+  const places = JSON.parse(await fs.readFile("result_orig.json", "utf8"));
+
+  for (let i = 0; i < places.length; i++) {
+    const coord1 = getLatLon(places[i]);
+    if (!coord1) return;
+
+    for (let j = 0; j < places.length; j++) {
+      const coord2 = getLatLon(places[j]);
+
+      if (!coord2) continue;
+      if (coord1.equals(coord2)) continue;
+
+      console.log(
+        "place1: ",
+        places[i].tags.name,
+        "place2: ",
+        places[j].tags.name,
+      );
+      if (!checkDistance(coord1, coord2)) {
+        places.splice(j, 1);
+      }
+    }
+  }
+
+  console.log("total places: ", places.length);
+
+  await fs.writeFile(
+    "result.json",
+    JSON.stringify(places, null, 2),
+    (error) => {
+      if (error) {
+        console.error(error);
+      }
+    },
+  );
+};
+
+main();
